b9e1059130514967a5671f6542f3edc58dca0017
dipeshyadav653 <147603571+dipeshyadav653@users.noreply.github.com>
2023-10-11 17:00:08 +0530

    text changes

--- TestscriptDIPS_1/TestscriptDIPS_1.cs
+++ TestscriptDIPS_1/TestscriptDIPS_1.cs
@@ -17,6 +17,7 @@ namespace TestscriptDIPS_1
 		/// <param name="engine">Link with SLAutomation process.</param>
 		public void Run(IEngine engine)
 		{
-			engine.GenerateInformation("Hello Mumbai World!");	
+			engine.GenerateInformation("Hello Mumbai World!");
+			engine.GenerateInformation("welcome #dmempowermumbai")
 	}
 }
